2da1d6b66765edc27c449d30fbd8ccd6575f1c4d
Mohamad <mohamad.y.s.e@gmail.com>
2018-05-30 17:29:05 +0200

    tested the methods

--- src/sigma/se/Main.java
+++ src/sigma/se/Main.java
@@ -5,22 +5,23 @@ public class Main {
 
     /**
      * this method transform array of size 9,9 to 9 arrays of 3,3 size
+     *
      * @param a: the array of 9,9 size
      * @return array of 3,3,9 size
      */
-    public static int[][][] transform( int[][] a ){
+    public static int[][][] transform(int[][] a) {
         int k;
         int b[][][] = new int[3][3][9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                a[i][j] = i + j;
+
                 //        System.out.print(i + "," + j);
 
                 //        k=3* (int) (i/3)+ (int) j/3;
                 //     k=  (i-i%3)/3;
-                k= i-i%3+ (j-j%3)/3;
-                b[i%3][j%3][k]=a[i][j] ;
+                k = i - i % 3 + (j - j % 3) / 3;
+                b[i % 3][j % 3][k] = a[i][j];
                 // System.out.print( "("+k+ ")" + "  ");
 
             }
@@ -30,11 +31,12 @@ public class Main {
 
     /**
      * prints array whose size 3,3,9
+     *
      * @param b
      */
-    public static void print339(int[][][] b){
+    public static void print339(int[][][] b) {
 
-        for(int m=0;m<9;m++) {
+        for (int m = 0; m < 9; m++) {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     System.out.print(b[i][j][m] + " ");
@@ -45,9 +47,8 @@ public class Main {
         }
 
 
-
-
     }
+
     public static void main(String[] args) {
 
         int a[][];
@@ -57,17 +58,21 @@ public class Main {
         a = new int[9][9];
         b = new int[3][3][9];
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++)
             for (int j = 0; j < 9; j++) {
-                a[i][j] = i + j;
+                a[i][j] =   j;
             }
 
-            b = transform(a);
-            print339(b);
 
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
 
+        System.out.println(".......................................");
+        b = transform(a);
+             print339(b);
     }
-
-
-    }
-
+}
